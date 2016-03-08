9eec82081b65f077fc083f784d916f67d751c1b9
Yoni <teamagam1@gmail.com>
2016-03-08 10:26:58 +0200

    added left drawer functionality

--- app/src/main/java/com/teamagam/gimelgimel/app/view/MainActivity.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/view/MainActivity.java
@@ -1,12 +1,26 @@
 package com.teamagam.gimelgimel.app.view;
 
+import android.app.Fragment;
+import android.app.SearchManager;
 import android.content.Intent;
-import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.teamagam.gimelgimel.R;
 import com.teamagam.gimelgimel.app.GGApplication;
@@ -15,9 +29,7 @@ import com.teamagam.gimelgimel.app.view.fragments.ViewerFragment;
 import com.teamagam.gimelgimel.app.view.fragments.FriendsFragment;
 import com.teamagam.gimelgimel.app.view.settings.SettingsActivity;
 
-//todo: clean
-//import com.commonsware.cwac.wakeful.WakefulIntentService;
-//import com.teamagam.gimelgimel.app.control.services.GGService;
+import java.util.Locale;
 
 public class MainActivity extends BaseActivity<GGApplication>
         implements ViewerFragment.OnFragmentInteractionListener {
@@ -26,11 +38,21 @@ public class MainActivity extends BaseActivity<GGApplication>
     private final String TAG_FRAGMENT_FRIENDS = TAG + "TAG_FRAGMENT_GG_FRIENDS";
     private final String TAG_FRAGMENT_MAP_CESIUM = TAG + "TAG_FRAGMENT_GG_CESIUM";
 
+    //drawer parameters
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private CharSequence mDrawerTitle;
+    private String[] mMenuTitles;
+
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
 
+
+    //app fragments
     private FriendsFragment mFriendsFragment;
     private ViewerFragment mViewerFragment;
 
@@ -54,9 +76,13 @@ public class MainActivity extends BaseActivity<GGApplication>
                     TAG_FRAGMENT_MAP_CESIUM);
         }
 
-        GGLocation gps = new GGLocation(this);
-        Location loc = gps.getLocation();
         mTitle = getTitle();
+
+        // creating the menu of the left side
+        createLeftDrawer();
+
+        // calculating current gps location
+        CalculateCurrentLocation();
 
         //todo: start both fragments.
         // Now do the actual swap of views
@@ -64,31 +90,102 @@ public class MainActivity extends BaseActivity<GGApplication>
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, mViewerFragment, TAG_FRAGMENT_MAP_CESIUM)
                     .commit();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, mFriendsFragment, TAG_FRAGMENT_FRIENDS)
+                    .commit();
         }
         else{
-
+            //// TODO: why do we need else???
         }
 
-//        R.id.fragment_friends_recycler_view
-
         //todo: where to start service? login activity?
-//        WakefulIntentService.sendWakefulWork(this, GGService.actionGetTipsIntent(this));
+        //WakefulIntentService.sendWakefulWork(this, GGService.actionGetTipsIntent(this));
+    }
 
+    private void CalculateCurrentLocation() {
+
+        GGLocation gps = new GGLocation(this);
+        mLocation = gps.getLocation();
+    }
+
+
+    private void createLeftDrawer()
+    {
+        mDrawerTitle = getTitle();
+        mMenuTitles = getResources().getStringArray(R.array.menu_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // set the images
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+
+        ((AppCompatActivity)this).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)this).getSupportActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                null,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        );
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
     }
 
-    public void restoreActionBar() {
-//        mToolbar.setTitle(mTitle);
+    public void onDrawerClosed(View view) {
+        getActionBar().setTitle(mTitle);
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+    }
+
+    public void onDrawerOpened(View drawerView) {
+        getActionBar().setTitle(mDrawerTitle);
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        // create the images in the menu
+        ImageView imageViewIcon = (ImageView) findViewById(R.id.imageViewIcon);
+        imageViewIcon.setImageResource(R.drawable.search);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+
         Intent intent;
         switch (item.getItemId()) {
             // action with ID action_settings was selected
@@ -103,8 +200,56 @@ public class MainActivity extends BaseActivity<GGApplication>
             default:
                 break;
         }
-        return super.onOptionsItemSelected(item);
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.action_websearch:
+                // create intent to perform web search for this menu
+                intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+                // catch event that there's no activity to handle intent
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new MenuFragment();
+        Bundle args = new Bundle();
+        args.putInt(MenuFragment.ARG_MENU_NUMBER, position);
+        fragment.setArguments(args);
+
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mMenuTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    public void restoreActionBar() {
+//        mToolbar.setTitle(mTitle);
+    }
+
 
     @Override
     protected int getActivityLayout() {
@@ -114,5 +259,30 @@ public class MainActivity extends BaseActivity<GGApplication>
     @Override
     public void onFragmentInteraction(Uri uri) {
         //todo: complete interaction with fragment.
+
+    }
+    /**
+         * Fragment that appears in the "content_frame", shows a menu
+         */
+    public static class MenuFragment extends Fragment {
+        public static final String ARG_MENU_NUMBER = "menu_number";
+
+        public MenuFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
+            int i = getArguments().getInt(ARG_MENU_NUMBER);
+            String menu = getResources().getStringArray(R.array.menu_array)[i];
+
+            int imageId = getResources().getIdentifier(menu.toLowerCase(Locale.getDefault()),
+                    "drawable", getActivity().getPackageName());
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(menu);
+            return rootView;
+        }
     }
 }
