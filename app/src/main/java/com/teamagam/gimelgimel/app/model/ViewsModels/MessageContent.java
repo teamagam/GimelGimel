0d0882ec50ea961d8a57cb87254c3e3587056fe7
Gil <gil.raytan.int@gmail.com>
2016-03-21 16:55:58 +0200

    added decleration of Message class

--- app/src/main/java/com/teamagam/gimelgimel/app/model/ViewsModels/MessageContent.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/model/ViewsModels/MessageContent.java
@@ -5,8 +5,42 @@ package com.teamagam.gimelgimel.app.model.ViewsModels;
  */
 public class MessageContent {
 
-    String mText;
-    Float mLongitute;
-    Float mLatitude;
+    String mText = null;
+    Float mLongitute = 0.0f;
+    Float mLatitude =0.0f ;
 
+
+    public MessageContent(String text)
+    {
+        this.mText = text;
+    }
+
+    public MessageContent(float longitute, float latitude)
+    {
+        this.mLatitude = longitute;
+        this.mLatitude = latitude;
+    }
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+
+    public Float getmLongitute() {
+        return mLongitute;
+    }
+
+    public void setmLongitute(Float mLongitute) {
+        this.mLongitute = mLongitute;
+    }
+
+    public Float getmLatitude() {
+        return mLatitude;
+    }
+
+    public void setmLatitude(Float mLatitude) {
+        this.mLatitude = mLatitude;
+    }
 }
