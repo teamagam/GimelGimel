de9d03199be6024e5d6a9e188f359bedadc74ed2
Dor <teamagam3@gmail.com>
2016-07-05 17:10:03 +0300

    Refectored MainActivity to fit better to android best parctices.
    Changed the layout of the MainActivity - More Material Designed.

--- app/src/main/java/com/teamagam/gimelgimel/app/view/fragments/MessagesFragment.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/view/fragments/MessagesFragment.java
@@ -14,64 +14,23 @@ import com.teamagam.gimelgimel.app.GGApplication;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MessageFragment.OnFragmentInteractionListener} interface
+ * {@link MessagesFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MessageFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class MessageFragment extends BaseFragment<GGApplication>{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MessagesFragment extends BaseFragment<GGApplication> {
 
     private OnFragmentInteractionListener mListener;
 
-    public MessageFragment() {
+    public MessagesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MessageFragment newInstance(String param1, String param2) {
-        MessageFragment fragment = new MessageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
-    }
-
-    @Override
-    protected int getFragmentLayout() {
-        return R.layout.fragment_message;
+        return inflater.inflate(R.layout.fragment_messages, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -96,6 +55,11 @@ public class MessageFragment extends BaseFragment<GGApplication>{
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_messages;
     }
 
     /**
