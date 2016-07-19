7e0ac523573208385a8e73a116a5163ebd8bdd8a
Dor <teamagam3@gmail.com>
2016-07-19 10:15:23 +0300

    Reverted unwanted changes

--- app/src/main/java/com/teamagam/gimelgimel/app/model/ViewsModels/Message.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/model/ViewsModels/Message.java
@@ -18,7 +18,7 @@ public abstract class Message<T> {
     @StringDef({TEXT, LAT_LONG, USER_LOCATION, IMAGE})
     public @interface MessageType {}
     public static final String TEXT = "Text";
-    public static final String LAT_LONG = "Geo";
+    public static final String LAT_LONG = "LatLong";
     public static final String USER_LOCATION = "UserLocation";
     public static final String IMAGE = "Image";
     @SerializedName("content")
