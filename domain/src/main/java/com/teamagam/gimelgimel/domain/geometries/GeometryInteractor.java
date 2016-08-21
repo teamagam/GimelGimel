706e57b336de769ea83fb32a34ac61dc74b9caaa
Dor <teamagam3@gmail.com>
2016-08-21 12:44:10 +0300

    Added basic interactor

---
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/geometries/GeometryInteractor.java
@@ -0,0 +1,19 @@
+package com.teamagam.gimelgimel.domain.geometries;
+
+import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
+import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
+import com.teamagam.gimelgimel.domain.base.interactors.AbstractInteractor;
+
+import rx.Observable;
+
+public class GeometryInteractor extends AbstractInteractor {
+    
+    protected GeometryInteractor(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+        super(threadExecutor, postExecutionThread);
+    }
+
+    @Override
+    protected Observable buildUseCaseObservable() {
+        return null;
+    }
+}
