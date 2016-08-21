56dd46d0253d17ce9bac935750d4a789b40c1756
Dor <teamagam3@gmail.com>
2016-08-21 16:44:34 +0300

    Finished GeometryInteractor
    Added basic presenter for geo message

--- domain/src/main/java/com/teamagam/gimelgimel/domain/geometries/GeometryInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/geometries/GeometryInteractor.java
@@ -3,17 +3,61 @@ package com.teamagam.gimelgimel.domain.geometries;
 import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
 import com.teamagam.gimelgimel.domain.base.interactors.AbstractInteractor;
+import com.teamagam.gimelgimel.domain.geometries.entities.GeoEntity;
+import com.teamagam.gimelgimel.domain.geometries.entities.VectorLayer;
+import com.teamagam.gimelgimel.domain.geometries.repository.GeoRepository;
+import com.teamagam.gimelgimel.domain.messages.entity.MessageGeo;
+import com.teamagam.gimelgimel.domain.messages.repository.MessagesRepository;
 
 import rx.Observable;
+import rx.Subscriber;
 
 public class GeometryInteractor extends AbstractInteractor {
-    
-    protected GeometryInteractor(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+
+    private static final String LAYER_ID = "UserLayer";
+
+    private GeoRepository mGeoRepository;
+    private MessagesRepository mMessagesRepository;
+    private GeoEntity mGeoEntity;
+    private MessageGeo mMessage;
+
+    protected GeometryInteractor(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread,
+                                 GeoRepository repository, MessagesRepository messagesRepository) {
         super(threadExecutor, postExecutionThread);
+
+        mGeoRepository = repository;
+        mMessagesRepository = messagesRepository;
+    }
+
+    public void addUserGeoEntity(GeoEntity entity, MessageGeo message, Subscriber subscriber) {
+        mGeoEntity = entity;
+        mMessage = message;
+
+        execute(subscriber);
     }
 
     @Override
     protected Observable buildUseCaseObservable() {
-        return null;
+        if (mGeoEntity == null || mMessage == null) {
+            throw new IllegalArgumentException("One of the following parameters is null: entity, message");
+        }
+
+        return mMessagesRepository.sendMessage(mMessage)
+                .doOnNext(mMessagesRepository::putMessage)
+                .doOnCompleted(() ->
+                        mGeoRepository.getVectorLayerById(LAYER_ID)
+                                .count()
+                                .subscribe(count -> {
+                                    if (count == 0) {
+                                        mGeoRepository.addVectorLayer(createUserVectorLayer());
+                                    }
+
+                                    mGeoRepository.addGeoEntityToVectorLayer(LAYER_ID, mGeoEntity);
+                                })
+                );
+    }
+
+    private VectorLayer createUserVectorLayer() {
+        return new VectorLayer(LAYER_ID);
     }
 }
---
+++ presentation/src/main/java/com/teamagam/gimelgimel/presentation/presenters/SendGeoEntityPresenter.java
@@ -0,0 +1,25 @@
+package com.teamagam.gimelgimel.presentation.presenters;
+
+import com.teamagam.gimelgimel.presentation.presenters.base.AbstractPresenter;
+
+public class SendGeoEntityPresenter extends AbstractPresenter {
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
