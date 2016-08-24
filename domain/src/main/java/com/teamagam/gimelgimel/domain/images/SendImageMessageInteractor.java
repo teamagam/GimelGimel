63e462493654df40a94cf05890ed3159e68dae8b
Dor <teamagam3@gmail.com>
2016-08-24 11:32:22 +0300

    Added basic classes

---
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/images/SendImageMessageInteractor.java
@@ -0,0 +1,19 @@
+package com.teamagam.gimelgimel.domain.images;
+
+import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
+import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
+import com.teamagam.gimelgimel.domain.base.interactors.AbstractInteractor;
+
+import rx.Observable;
+
+public class SendImageMessageInteractor extends AbstractInteractor {
+
+    public SendImageMessageInteractor(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+        super(threadExecutor, postExecutionThread);
+    }
+
+    @Override
+    protected Observable buildUseCaseObservable() {
+        return null;
+    }
+}
---
+++ presentation/src/main/java/com/teamagam/gimelgimel/presentation/presenters/SendImageMessagePresenter.java
@@ -0,0 +1,33 @@
+package com.teamagam.gimelgimel.presentation.presenters;
+
+import com.teamagam.gimelgimel.presentation.presenters.base.AbstractPresenter;
+
+import javax.inject.Inject;
+
+public class SendImageMessagePresenter extends AbstractPresenter {
+
+    @Inject
+    public SendImageMessagePresenter() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
