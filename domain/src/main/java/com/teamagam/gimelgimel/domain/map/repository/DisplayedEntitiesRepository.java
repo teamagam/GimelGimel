990b6f2683215421aa3935c2c4b941a67caa080f
Dor <teamagam3@gmail.com>
2016-10-09 09:22:05 +0300

    Fixed file name

---
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/map/repository/DisplayedEntitiesRepository.java
@@ -0,0 +1,23 @@
+package com.teamagam.gimelgimel.domain.map.repository;
+
+import com.teamagam.gimelgimel.domain.map.entities.mapEntities.GeoEntity;
+import com.teamagam.gimelgimel.domain.notifications.entity.GeoEntityNotification;
+
+import java.util.Collection;
+
+import rx.Observable;
+
+/**
+ * repository to be synced with the app for shown entities on the map
+ */
+
+public interface DisplayedEntitiesRepository {
+
+    Observable<GeoEntityNotification> getSyncEntitiesObservable();
+
+    Observable<Collection<GeoEntity>> getDisplayedGeoEntitiesObservable();
+
+    void show(GeoEntity geoEntity);
+
+    void hide(GeoEntity geoEntity);
+}
