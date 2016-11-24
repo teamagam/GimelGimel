275f09535f8edebb7a9e0943a382a2833940449b
Yoni <teamagam1@gmail.com>
2016-11-24 11:30:25 +0200

    rebase fixes

--- domain/src/main/java/com/teamagam/gimelgimel/domain/map/entities/interfaces/ISymbolVisitor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/map/entities/interfaces/ISymbolVisitor.java
@@ -1,6 +1,7 @@
 package com.teamagam.gimelgimel.domain.map.entities.interfaces;
 
 import com.teamagam.gimelgimel.domain.map.entities.symbols.ImageSymbol;
+import com.teamagam.gimelgimel.domain.map.entities.symbols.MyLocationSymbol;
 import com.teamagam.gimelgimel.domain.map.entities.symbols.PointSymbol;
 import com.teamagam.gimelgimel.domain.map.entities.symbols.UserSymbol;
 
