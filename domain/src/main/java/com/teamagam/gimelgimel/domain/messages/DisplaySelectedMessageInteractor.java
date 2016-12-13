eb4ddc62cf4c42c3498e1af39e3c0912d1d84527
Bar <barmako@gmail.com>
2016-12-13 15:04:11 +0200

    Refactoring interactors to share code when subscribing and unsubscribing from multiple (or single) observable. Most of the sync interactors have been refactored, with the exception of few "bad" interactors that needs to change in the future. Do interactors have not been refactored, but a new base class that supports the above refactoring have been implemented - BaseDataInteractor. AbsInteractor, SyncInteractor and DoInteractor needs to be deleted when these changes will be adopted completely.

--- domain/src/main/java/com/teamagam/gimelgimel/domain/messages/DisplaySelectedMessageInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/messages/DisplaySelectedMessageInteractor.java
@@ -4,52 +4,36 @@ import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
-import com.teamagam.gimelgimel.domain.base.interactors.Interactor;
-import com.teamagam.gimelgimel.domain.base.subscribers.SimpleSubscriber;
+import com.teamagam.gimelgimel.domain.base.interactors.BaseDisplayInteractor;
+import com.teamagam.gimelgimel.domain.base.interactors.DisplaySubscriptionRequest;
 import com.teamagam.gimelgimel.domain.messages.entity.Message;
 import com.teamagam.gimelgimel.domain.messages.repository.MessagesRepository;
 
-import rx.Subscription;
+import java.util.Collections;
 
 @AutoFactory
-public class DisplaySelectedMessageInteractor implements Interactor {
+public class DisplaySelectedMessageInteractor extends BaseDisplayInteractor {
 
 
-    private final ThreadExecutor mThreadExecutor;
-    private final PostExecutionThread mPostExecutionThread;
     private final Displayer mDisplayer;
     private MessagesRepository mMessagesRepository;
-    private Subscription mSubscription;
 
     public DisplaySelectedMessageInteractor(
             @Provided ThreadExecutor threadExecutor,
             @Provided PostExecutionThread postExecutionThread,
             @Provided MessagesRepository messagesRepository,
             Displayer displayer) {
-        mThreadExecutor = threadExecutor;
-        mPostExecutionThread = postExecutionThread;
+        super(threadExecutor, postExecutionThread);
         mMessagesRepository = messagesRepository;
         mDisplayer = displayer;
     }
 
     @Override
-    public void execute() {
-        mSubscription = mMessagesRepository.getSelectedMessageObservable()
-                .subscribeOn(mThreadExecutor.getScheduler())
-                .observeOn(mPostExecutionThread.getScheduler())
-                .subscribe(new SimpleSubscriber<Message>() {
-                    @Override
-                    public void onNext(Message message) {
-                        mDisplayer.display(message);
-                    }
-                });
-    }
-
-    @Override
-    public void unsubscribe() {
-        if (mSubscription != null && !mSubscription.isUnsubscribed()) {
-            mSubscription.unsubscribe();
-        }
+    protected Iterable<SubscriptionRequest> buildSubscriptionRequests(
+            DisplaySubscriptionRequest.DisplaySubscriptionRequestFactory factory) {
+        return Collections.singletonList(
+                factory.create(mMessagesRepository.getMessagesObservable(),
+                        mDisplayer::display));
     }
 
     public interface Displayer {
