0e0c0691c060686b095ea97dc570269e7b1b9ce9
Gil <teamagam4@gmail.com>
2016-12-26 17:13:48 +0200

    starting implementing display bubble alert

--- app/src/main/java/com/teamagam/gimelgimel/app/mainActivity/view/MainActivityAlerts.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/mainActivity/view/MainActivityAlerts.java
@@ -1,9 +1,15 @@
 package com.teamagam.gimelgimel.app.mainActivity.view;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
+import com.nineoldandroids.animation.ArgbEvaluator;
+import com.nineoldandroids.animation.ValueAnimator;
 import com.teamagam.gimelgimel.R;
 import com.teamagam.gimelgimel.app.common.base.view.ActivitySubcomponent;
 import com.teamagam.gimelgimel.app.common.logging.AppLogger;
@@ -25,6 +31,9 @@ public class MainActivityAlerts extends ActivitySubcomponent implements AlertsVi
     TextView mNoGpsTextView;
     @BindView(R.id.no_network_text_view)
     TextView mNoNetworkTextView;
+    @BindView(R.id.main_toolbar)
+    Toolbar mToolBar;
+
 
     @Inject
     AlertsViewModel mViewModel;
@@ -68,6 +77,26 @@ public class MainActivityAlerts extends ActivitySubcomponent implements AlertsVi
     private void displayAlertTextView(TextView textview) {
         textview.setVisibility(View.VISIBLE);
         textview.bringToFront();
+    }
+
+    private void changeToolBarColorForNewBubbleAlert() {
+
+        Integer colorFrom = ContextCompat.getColor(mActivity.getBaseContext(), R.color.blue);
+        Integer colorTo = ContextCompat.getColor(mActivity.getBaseContext(), R.color.red);
+
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                mToolBar.setBackgroundColor((Integer) animator.getAnimatedValue());
+            }
+        });
+
+        colorAnimation.setDuration(1000);
+        colorAnimation.setStartDelay(0);
+        colorAnimation.start();
     }
 
     private void hideAlertTextView(TextView textView) {
---
+++ data/src/main/java/com/teamagam/gimelgimel/data/notifications/BubbleAlerts.java
@@ -0,0 +1,8 @@
+package com.teamagam.gimelgimel.data.notifications;
+
+/**
+ * Created by CV2 on 12/26/2016.
+ */
+
+public class BubbleAlerts {
+}
