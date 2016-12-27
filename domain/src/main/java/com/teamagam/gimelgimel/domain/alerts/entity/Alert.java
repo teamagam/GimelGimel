fa18011fc7ce1ee0fd5001cca6065f0fc8c58a95
Gil <teamagam4@gmail.com>
2016-12-27 16:14:43 +0200

    Support basic Alert. 1. Alert entity created and its support:    a. AlertMessage    b. AlertData    c. Transformers 2. Alerts repository. 3. Alerts messages are added to the repo.

--- domain/src/main/java/com/teamagam/gimelgimel/domain/alerts/entity/Alert.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/alerts/entity/Alert.java
@@ -1,7 +1,5 @@
 package com.teamagam.gimelgimel.domain.alerts.entity;
 
-import com.teamagam.gimelgimel.domain.map.entities.mapEntities.AlertEntity;
-
 public class Alert {
 
     public static String TYPE_BUBBLE = "bubble";
@@ -12,19 +10,13 @@ public class Alert {
     private final String mText;
     private final int mSeverity;
     private final String mMessageId;
-    private final AlertEntity mEntity;
 
-    public Alert(String source, long time, String text, int severity, String messageId, AlertEntity entity) {
+    public Alert(String source, long time, String text, int severity, String messageId) {
         mSource = source;
         mTime = time;
         mText = text;
         mSeverity = severity;
         mMessageId = messageId;
-        mEntity = entity;
-    }
-
-    public AlertEntity getEntity() {
-        return mEntity;
     }
 
     public String getSource() {
