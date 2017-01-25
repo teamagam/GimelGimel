b64996a7dea2270748e507aa3d7134b6684fa78c
Bar <barmako@gmail.com>
2017-01-25 11:14:51 +0200

    New incoming VectorLayers are processing now via an interactor. This interactor (ProcessIncomingVectorLayersInteractor) makes sure the vector layer is cached to the device's disk.

--- app/src/main/java/com/teamagam/gimelgimel/app/injectors/modules/RepositoryModule.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/injectors/modules/RepositoryModule.java
@@ -137,12 +137,4 @@ public class RepositoryModule {
             SelectedSensorDataRepository selectedSensorDataRepository) {
         return selectedSensorDataRepository;
     }
-
-
-    //TODO: this probably shouldn't be here.
-    @Provides
-    @Singleton
-    LayersLocalCache provideLayersLocalCache(LayersLocalCacheData layersLocalCacheData){
-        return layersLocalCacheData;
-    }
 }
