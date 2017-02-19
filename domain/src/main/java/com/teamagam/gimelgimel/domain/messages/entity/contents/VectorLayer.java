6b9d5c65e2b09132a84887ef0582004bfa77e954
Bar <barmako@gmail.com>
2017-02-19 17:33:38 +0200

    Added severity to VectorLayers. By default (constructor), VLs will be creation with "regular" severity. As of now, a layer coming from the server will be considered "important" unless specified otherwise in it's JSON (severity : "regular", case-insensitive).

--- domain/src/main/java/com/teamagam/gimelgimel/domain/messages/entity/contents/VectorLayer.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/messages/entity/contents/VectorLayer.java
@@ -5,11 +5,17 @@ public class VectorLayer {
     private final String mId;
     private String mName;
     private int mVersion;
+    private Severity mSeverity;
 
     public VectorLayer(String id, String name, int version) {
+        this(id, name, version, Severity.REGULAR);
+    }
+
+    public VectorLayer(String id, String name, int version, Severity severity) {
         mId = id;
         mName = name;
         mVersion = version;
+        mSeverity = severity;
     }
 
     public String getId() {
@@ -24,8 +30,16 @@ public class VectorLayer {
         return mVersion;
     }
 
+    public boolean isImportant() {
+        return mSeverity == Severity.IMPORTANT;
+    }
+
     @Override
     public String toString() {
         return String.format("ID=%s \r\n NAME=%s \r\n VERSION=%d \r\n", mId, mName, mVersion);
+    }
+
+    public enum Severity {
+        REGULAR, IMPORTANT
     }
 }
