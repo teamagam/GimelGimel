58c92ede023dbe97b12d81d091b618b55c3cc6e0
IdoAgam <teamagam5@gmail.com>
2017-03-21 13:47:44 +0200

    CR Fixes

--- data/src/main/java/com/teamagam/gimelgimel/data/location/repository/UsersLocationDataRepository.java
+++ data/src/main/java/com/teamagam/gimelgimel/data/location/repository/UsersLocationDataRepository.java
@@ -3,8 +3,9 @@ package com.teamagam.gimelgimel.data.location.repository;
 import com.teamagam.gimelgimel.domain.location.entity.UserLocation;
 import com.teamagam.gimelgimel.domain.location.respository.UsersLocationRepository;
 import com.teamagam.gimelgimel.domain.messages.entity.contents.LocationSample;
-import com.teamagam.gimelgimel.domain.utils.SerializedSubjectBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.TreeMap;
@@ -12,49 +13,35 @@ import java.util.TreeMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import rx.Observable;
-import rx.subjects.SerializedSubject;
-
 @Singleton
 public class UsersLocationDataRepository implements UsersLocationRepository {
 
     private Map<String, Stack<LocationSample>> mUsersLocations;
 
-    private final SerializedSubject<UserLocation, UserLocation> mUsersLocationSubject;
-
     @Inject
     public UsersLocationDataRepository() {
         mUsersLocations = new TreeMap<>();
-
-        mUsersLocationSubject = new SerializedSubjectBuilder().build();
     }
 
     @Override
-    public void add(String userId, LocationSample lastUserLocation) {
+    public void add(String userId, LocationSample lastUserSample) {
         if (!mUsersLocations.containsKey(userId)) {
             mUsersLocations.put(userId, new Stack<>());
         }
-        mUsersLocations.get(userId).add(lastUserLocation);
-        mUsersLocationSubject.onNext(createUserLocation(userId, lastUserLocation));
+        mUsersLocations.get(userId).add(lastUserSample);
     }
 
     @Override
-    public Observable<UserLocation> getUsersLocationUpdates() {
-        return mUsersLocationSubject.asObservable();
-    }
-
-    @Override
-    public Observable<UserLocation> getLastLocations() {
-        return rx.Observable.from(mUsersLocations.entrySet())
-                .map(this::extractLastLocation);
+    public Iterable<UserLocation> getLastLocations() {
+        List<UserLocation> list = new ArrayList<>();
+        for (Map.Entry<String, Stack<LocationSample>> entry : mUsersLocations.entrySet()) {
+            list.add(extractLastLocation(entry));
+        }
+        return list;
     }
 
     private UserLocation extractLastLocation(Map.Entry<String, Stack<LocationSample>> entry) {
-        return createUserLocation(entry.getKey(), entry.getValue().peek());
-    }
-
-    private UserLocation createUserLocation(String userId, LocationSample locationSample) {
-        return new UserLocation(userId, locationSample);
+        return new UserLocation(entry.getKey(), entry.getValue().peek());
     }
 
 }
--- domain/src/main/java/com/teamagam/gimelgimel/domain/config/Constants.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/config/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final long USER_LOCATION_STALE_THRESHOLD_MS = 60 * 1000;
     public static final long USER_LOCATION_RELEVANCE_THRESHOLD_MS = 12 * 60 * 60 * 1000;
 
-    public static final long USERS_LOCATION_REFRESH_FREQUENCY_MS = 60 * 1000;
+    public static final long USERS_LOCATION_REFRESH_FREQUENCY_MS = 5 * 1000;
 
     public static final int LAYER_CACHING_RETRIES = 3;
     public static final long LAYER_CACHING_RETRIES_DELAY_MS = 2 * 1000;
