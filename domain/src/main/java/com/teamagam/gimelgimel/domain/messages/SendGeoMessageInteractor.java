2ddeebdf86fa7bd5694ce8608d0a475badc33886
IdoAgam <teamagam5@gmail.com>
2017-03-23 15:23:58 +0200

    Refactored SendGeoMessageInteractor to have a visitor that handles PointGeometry and Polygon.

--- domain/src/main/java/com/teamagam/gimelgimel/domain/messages/SendGeoMessageInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/messages/SendGeoMessageInteractor.java
@@ -3,10 +3,15 @@ package com.teamagam.gimelgimel.domain.messages;
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
+import com.teamagam.gimelgimel.domain.map.entities.geometries.Geometry;
 import com.teamagam.gimelgimel.domain.map.entities.geometries.PointGeometry;
+import com.teamagam.gimelgimel.domain.map.entities.geometries.Polygon;
+import com.teamagam.gimelgimel.domain.map.entities.interfaces.IGeometryVisitor;
 import com.teamagam.gimelgimel.domain.map.entities.mapEntities.GeoEntity;
 import com.teamagam.gimelgimel.domain.map.entities.mapEntities.PointEntity;
+import com.teamagam.gimelgimel.domain.map.entities.mapEntities.PolygonEntity;
 import com.teamagam.gimelgimel.domain.map.entities.symbols.PointSymbol;
+import com.teamagam.gimelgimel.domain.map.entities.symbols.PolygonSymbol;
 import com.teamagam.gimelgimel.domain.messages.entity.MessageGeo;
 import com.teamagam.gimelgimel.domain.messages.repository.MessagesRepository;
 import com.teamagam.gimelgimel.domain.notifications.repository.MessageNotifications;
@@ -16,8 +21,10 @@ import com.teamagam.gimelgimel.domain.user.repository.UserPreferencesRepository;
 public class SendGeoMessageInteractor extends SendMessageInteractor<MessageGeo> {
 
     private final String mMessageText;
-    private final PointGeometry mMessageGeometry;
+    private final Geometry mMessageGeometry;
     private final String mMessageType;
+
+    private GeoEntity mGeoEntity;
 
     SendGeoMessageInteractor(
             @Provided ThreadExecutor threadExecutor,
@@ -25,25 +32,34 @@ public class SendGeoMessageInteractor extends SendMessageInteractor<MessageGeo> 
             @Provided MessagesRepository messagesRepository,
             @Provided MessageNotifications messageNotifications,
             String text,
-            PointGeometry pg,
+            Geometry geometry,
             String type) {
         super(threadExecutor, userPreferences, messageNotifications, messagesRepository);
         mMessageText = text;
-        mMessageGeometry = pg;
+        mMessageGeometry = geometry;
         mMessageType = type;
     }
 
     @Override
     protected MessageGeo createMessage(String senderId) {
-        return new MessageGeo(null, senderId, null, createGeoEntity());
+        mMessageGeometry.accept(new CreateGeoEntityVisitor());
+        return new MessageGeo(null, senderId, null, mGeoEntity);
     }
 
-    private GeoEntity createGeoEntity() {
-        PointSymbol symbol = createSymbolFromType(mMessageType);
-        return new PointEntity("not_used", mMessageText, mMessageGeometry, symbol);
-    }
+    private class CreateGeoEntityVisitor implements IGeometryVisitor {
 
-    private PointSymbol createSymbolFromType(String type) {
-        return new PointSymbol(false, type);
+        @Override
+        public void visit(PointGeometry pointGeometry) {
+            PointSymbol symbol = new PointSymbol(false, mMessageType);
+            mGeoEntity = new PointEntity(
+                    "not_used", mMessageText, pointGeometry, symbol);
+        }
+
+        @Override
+        public void visit(Polygon polygon) {
+            PolygonSymbol symbol = new PolygonSymbol(false);
+            mGeoEntity = new PolygonEntity(
+                    "not_used", mMessageText, polygon, symbol);
+        }
     }
 }
