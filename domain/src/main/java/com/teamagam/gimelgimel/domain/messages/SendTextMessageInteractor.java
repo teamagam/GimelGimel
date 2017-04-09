0da28438ecd3469c0c4d886f22c307bcba2c0cdc
Bar <barmako@gmail.com>
2017-04-09 11:29:24 +0300

    Fixed trimming bug that caused all newlines to be removed from messages text contents (even from within the string)

--- domain/src/main/java/com/teamagam/gimelgimel/domain/messages/SendTextMessageInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/messages/SendTextMessageInteractor.java
@@ -7,7 +7,6 @@ import com.teamagam.gimelgimel.domain.messages.entity.MessageText;
 import com.teamagam.gimelgimel.domain.messages.repository.MessagesRepository;
 import com.teamagam.gimelgimel.domain.notifications.repository.MessageNotifications;
 import com.teamagam.gimelgimel.domain.user.repository.UserPreferencesRepository;
-import com.teamagam.gimelgimel.domain.utils.TextUtils;
 
 @AutoFactory
 public class SendTextMessageInteractor extends SendMessageInteractor<MessageText> {
@@ -26,6 +25,6 @@ public class SendTextMessageInteractor extends SendMessageInteractor<MessageText
 
     @Override
     protected MessageText createMessage(String senderId) {
-        return new MessageText(null, senderId, null, TextUtils.trim(mText));
+        return new MessageText(null, senderId, null, mText.trim());
     }
 }
