31e05df99b879432df99df6be235a2d0ba2d37d6
Bar <barmako@gmail.com>
2017-05-15 12:15:01 +0300

    Filtering self-location server updates. SelfLocationUpdates will be sent to the server only if the new sample meets at least one of the following conditions:  1. New location sample time is newer than the last sample by 20 seconds  2. Distance delta between the the new sample and the last sent sample is more than 25 meters.

--- domain/src/main/java/com/teamagam/gimelgimel/domain/location/SendSelfLocationsInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/location/SendSelfLocationsInteractor.java
@@ -6,6 +6,7 @@ import com.teamagam.gimelgimel.domain.base.interactors.BaseDataInteractor;
 import com.teamagam.gimelgimel.domain.base.interactors.DataSubscriptionRequest;
 import com.teamagam.gimelgimel.domain.config.Constants;
 import com.teamagam.gimelgimel.domain.location.respository.LocationRepository;
+import com.teamagam.gimelgimel.domain.map.SpatialEngine;
 import com.teamagam.gimelgimel.domain.messages.entity.MessageUserLocation;
 import com.teamagam.gimelgimel.domain.messages.entity.contents.LocationSample;
 import com.teamagam.gimelgimel.domain.messages.repository.MessagesRepository;
@@ -18,16 +19,19 @@ import javax.inject.Inject;
 public class SendSelfLocationsInteractor extends BaseDataInteractor {
 
     private UserPreferencesRepository mUserPreferences;
+    private SpatialEngine mSpatialEngine;
     private MessagesRepository mMessagesRepository;
     private LocationRepository mLocationRepository;
 
     @Inject
     public SendSelfLocationsInteractor(ThreadExecutor threadExecutor,
                                        UserPreferencesRepository userPreferences,
+                                       SpatialEngine spatialEngine,
                                        MessagesRepository messagesRepository,
                                        LocationRepository locationRepository) {
         super(threadExecutor);
         mUserPreferences = userPreferences;
+        mSpatialEngine = spatialEngine;
         mMessagesRepository = messagesRepository;
         mLocationRepository = locationRepository;
     }
@@ -35,7 +39,6 @@ public class SendSelfLocationsInteractor extends BaseDataInteractor {
     @Override
     protected Iterable<SubscriptionRequest> buildSubscriptionRequests(
             DataSubscriptionRequest.SubscriptionRequestFactory factory) {
-
         return Collections.singletonList(buildSendRequest(factory));
     }
 
@@ -45,9 +48,20 @@ public class SendSelfLocationsInteractor extends BaseDataInteractor {
                 mLocationRepository.getLocationObservable(),
                 locationSampleObservable ->
                         locationSampleObservable
+                                .filter(this::shouldUpdateServer)
                                 .map(this::createMessage)
                                 .flatMap(mMessagesRepository::sendMessage)
         );
+    }
+
+    private boolean shouldUpdateServer(LocationSample locationSample) {
+        LocationSample lastLocationSample = mLocationRepository.getLastLocationSample();
+        long timeDeltaMs = locationSample.getTime() - lastLocationSample.getTime();
+        double distanceDeltaMeters = mSpatialEngine.distanceInMeters(
+                locationSample.getLocation(),
+                lastLocationSample.getLocation());
+        return timeDeltaMs > Constants.LOCATION_TIME_CHANGE_SERVER_UPDATE_THRESHOLD ||
+                distanceDeltaMeters > Constants.LOCATION_CHANGE_METERS_SERVER_UPDATE_THRESHOLD;
     }
 
     private MessageUserLocation createMessage(LocationSample locationSample) {
