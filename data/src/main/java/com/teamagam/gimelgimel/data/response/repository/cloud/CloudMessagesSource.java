58e3cb646da1eb39763e0a2d006d00d43bf71347
IdoAgam <teamagam5@gmail.com>
2017-06-12 10:22:36 +0300

    Fixed race condition of binding ImageMessage's GeoPanel to its displayed (or not yet :) ) entity. Renamed SimpleObserver to DummyObserver in honor of Uncle Bob's Episode 23: mocking.

--- data/src/main/java/com/teamagam/gimelgimel/data/response/repository/cloud/CloudMessagesSource.java
+++ data/src/main/java/com/teamagam/gimelgimel/data/response/repository/cloud/CloudMessagesSource.java
@@ -6,7 +6,7 @@ import com.teamagam.gimelgimel.data.response.entity.ConfirmMessageReadResponse;
 import com.teamagam.gimelgimel.data.response.entity.ServerResponse;
 import com.teamagam.gimelgimel.data.response.entity.ImageMessageResponse;
 import com.teamagam.gimelgimel.data.response.rest.GGMessagingAPI;
-import com.teamagam.gimelgimel.domain.base.subscribers.SimpleObserver;
+import com.teamagam.gimelgimel.domain.base.subscribers.DummyObserver;
 import java.io.File;
 import java.util.List;
 import javax.inject.Inject;
@@ -37,7 +37,7 @@ public class CloudMessagesSource {
   }
 
   public void informReadMessage(ConfirmMessageReadResponse confirmRead) {
-    mMessagingApi.informReadMessage(confirmRead).subscribe(new SimpleObserver<>());
+    mMessagingApi.informReadMessage(confirmRead).subscribe(new DummyObserver<>());
   }
 
   private Observable<ServerResponse> sendImage(ImageMessageResponse imageMessage) {
