dc9d1050613b027ec2b918fe8f386119c09b92fe
Bar <barmako@gmail.com>
2017-06-12 14:24:41 +0300

    Completed drawer new UX.
    All the drawer lists have their own adapter that is used when the tab is changed.

---
+++ app/src/main/java/com/teamagam/gimelgimel/app/mainActivity/drawer/adapters/RastersRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.teamagam.gimelgimel.app.mainActivity.drawer.adapters;
+
+import android.view.View;
+import com.teamagam.gimelgimel.R;
+import com.teamagam.gimelgimel.app.common.base.adapters.BaseRecyclerArrayAdapter;
+import com.teamagam.gimelgimel.app.common.base.adapters.IdentifiedData;
+import com.teamagam.gimelgimel.app.mainActivity.drawer.SimpleDrawerViewHolder;
+import com.teamagam.gimelgimel.domain.rasters.DisplayIntermediateRastersInteractor;
+import java.util.Comparator;
+
+public class RastersRecyclerAdapter extends
+    BaseRecyclerArrayAdapter<SimpleDrawerViewHolder, RastersRecyclerAdapter.IdentifiedRasterAdapter> {
+
+  public RastersRecyclerAdapter(OnItemClickListener<IdentifiedRasterAdapter> onItemClickListener) {
+    super(IdentifiedRasterAdapter.class, new RastersComparator(), onItemClickListener);
+  }
+
+  @Override
+  protected SimpleDrawerViewHolder createNewViewHolder(View v, int viewType) {
+    return new SimpleDrawerViewHolder(v);
+  }
+
+  @Override
+  protected void bindItemToView(SimpleDrawerViewHolder holder, IdentifiedRasterAdapter data) {
+    holder.setText(data.getName());
+    holder.setTextColor(getTextColor(data));
+  }
+
+  @Override
+  protected int getListItemLayout(int viewType) {
+    return R.layout.drawer_list_item;
+  }
+
+  private int getTextColor(IdentifiedRasterAdapter data) {
+    if (data.isShown()) {
+      return R.color.colorPrimaryDark;
+    }
+    return R.color.secondaryText;
+  }
+
+  public static class IdentifiedRasterAdapter
+      extends DisplayIntermediateRastersInteractor.IntermediateRasterPresentation
+      implements IdentifiedData {
+
+    public IdentifiedRasterAdapter(DisplayIntermediateRastersInteractor.IntermediateRasterPresentation raster) {
+      super(raster.getName(), raster.getUri(), raster.isShown());
+    }
+
+    @Override
+    public String getId() {
+      return getName();
+    }
+  }
+
+  private static class RastersComparator implements Comparator<IdentifiedRasterAdapter> {
+    @Override
+    public int compare(IdentifiedRasterAdapter lhs, IdentifiedRasterAdapter rhs) {
+      return lhs.getName().compareTo(rhs.getName());
+    }
+  }
+}
