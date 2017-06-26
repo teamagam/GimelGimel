73cbe637a755fe7ac5b30734614b253feb1e4e8a
Bar <barmako@gmail.com>
2017-06-26 15:43:28 +0300

    App notifications now support different (user-configurable) sounds and vibration.

--- app/src/main/java/com/teamagam/gimelgimel/app/injectors/modules/ApplicationModule.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/injectors/modules/ApplicationModule.java
@@ -9,9 +9,7 @@ import com.teamagam.gimelgimel.app.common.rx.schedulers.DataThread;
 import com.teamagam.gimelgimel.app.common.rx.schedulers.UIThread;
 import com.teamagam.gimelgimel.app.common.utils.Constants;
 import com.teamagam.gimelgimel.app.map.esri.EsriExtentResolver;
-import com.teamagam.gimelgimel.app.common.utils.Environment;
 import com.teamagam.gimelgimel.app.map.esri.EsriSpatialEngine;
-import com.teamagam.gimelgimel.app.notifications.AppNotifier;
 import com.teamagam.gimelgimel.data.location.LocationFetcher;
 import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
@@ -95,11 +93,5 @@ public class ApplicationModule {
   @Provides
   IntermediateRasterExtentResolver provideIntermediateRasterExtentResolver(EsriExtentResolver esriExtentResolver) {
     return esriExtentResolver;
-  }
-
-  @Provides
-  @Singleton
-  AppNotifier provideAppNotifier() {
-    return new AppNotifier(mApplication);
   }
 }
--- app/src/main/java/com/teamagam/gimelgimel/app/notifications/AppNotifier.java
+++ app/src/main/java/com/teamagam/gimelgimel/app/notifications/AppNotifier.java
@@ -1,10 +1,12 @@
 package com.teamagam.gimelgimel.app.notifications;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import com.teamagam.gimelgimel.R;
 import com.teamagam.gimelgimel.app.common.utils.Constants;
@@ -12,15 +14,21 @@ import com.teamagam.gimelgimel.app.mainActivity.view.MainActivity;
 import com.teamagam.gimelgimel.domain.messages.entity.ChatMessage;
 import com.teamagam.gimelgimel.domain.messages.entity.features.AlertFeature;
 import com.teamagam.gimelgimel.domain.notifications.NotifyOnNewMessageInteractor;
+import java.util.Objects;
+import javax.inject.Inject;
 
 import static android.support.v4.app.NotificationCompat.PRIORITY_MAX;
 
 public class AppNotifier implements NotifyOnNewMessageInteractor.NotificationDisplayer {
 
-  private static final long VIBRATION_START_DELAY = 0;
-  private static final long VIBRATION_DURATION = 1500;
+  private static final long VIBRATION_START_DELAY_MS = 0;
+  private static final long VIBRATION_DURATION_MS = 1500;
   private static final long[] VIBRATION_PATTERN =
-      new long[] { VIBRATION_START_DELAY, VIBRATION_DURATION };
+      new long[] { VIBRATION_START_DELAY_MS, VIBRATION_DURATION_MS };
+
+  private final SharedPreferences mSharedPreferences;
+  private final Context mContext;
+  private final Intent mMainActivityIntent;
 
   private String mAlertTitle;
   private String mAlertSummary;
@@ -28,20 +36,19 @@ public class AppNotifier implements NotifyOnNewMessageInteractor.NotificationDis
   private String mMessageTitle;
   private String mMessageSummary;
   private String mMessageText;
-  private NotificationManager mNotificationManager;
-  private NotificationCompat.Builder mBuilder;
+  private String mShouldVibratePrefKey;
+  private String mRingtonePrefKey;
 
+  private NotificationManager mNotificationManager;
+
+  @Inject
   public AppNotifier(Context context) {
-    initializeNotificationStrings(context);
+    mContext = context;
+    initStringResources(mContext);
     mNotificationManager =
         (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-    Intent resultIntent = new Intent(context, MainActivity.class);
-
-    mBuilder = new NotificationCompat.Builder(context).setContentIntent(
-        PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT))
-        .setVibrate(VIBRATION_PATTERN)
-        .setPriority(PRIORITY_MAX)
-        .setAutoCancel(true);
+    mMainActivityIntent = new Intent(context, MainActivity.class);
+    mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
   }
 
   @Override
@@ -53,41 +60,71 @@ public class AppNotifier implements NotifyOnNewMessageInteractor.NotificationDis
     }
   }
 
-  private void initializeNotificationStrings(Context context) {
-    initializeAlertStrings(context);
-    initializeMessageStrings(context);
+  private void initStringResources(Context context) {
+    initAlertStrings(context);
+    initMessageStrings(context);
+    initPrefKeys(context);
   }
 
-  private void initializeAlertStrings(Context context) {
+  private void initAlertStrings(Context context) {
     mAlertTitle = context.getString(R.string.alert_notification_title);
     mAlertSummary = context.getString(R.string.alert_notification_summary);
     mAlertText = context.getString(R.string.alert_notification_text);
   }
 
-  private void initializeMessageStrings(Context context) {
+  private void initMessageStrings(Context context) {
     mMessageTitle = context.getString(R.string.message_notification_title);
     mMessageSummary = context.getString(R.string.message_notification_summary);
     mMessageText = context.getString(R.string.message_notification_text);
   }
 
-  private void notifyAlert() {
-    mBuilder.setSmallIcon(R.drawable.ic_alert_notification);
-    mBuilder.setStyle(createStyle(mAlertTitle, mAlertSummary, mAlertText));
-    mBuilder.setContentTitle(mAlertTitle);
-    mBuilder.setContentText(mAlertText);
+  private void initPrefKeys(Context context) {
+    mShouldVibratePrefKey = context.getString(R.string.pref_notifications_vibration_key);
+    mRingtonePrefKey = context.getString(R.string.pref_notifications_ringtone_key);
+  }
 
-    Notification n = mBuilder.build();
-    mNotificationManager.notify(Constants.ALERTS_NOTIFICATION_ID, n);
+  private void notifyAlert() {
+    notify(R.drawable.ic_alert_notification, mAlertTitle, mAlertSummary, mAlertText,
+        Constants.ALERTS_NOTIFICATION_ID);
   }
 
   private void notifyMessage() {
-    mBuilder.setSmallIcon(R.drawable.ic_lambda_logo);
-    mBuilder.setStyle(createStyle(mMessageTitle, mMessageSummary, mMessageText));
-    mBuilder.setContentTitle(mMessageTitle);
-    mBuilder.setContentText(mMessageText);
+    notify(R.drawable.ic_lambda_logo, mMessageTitle, mMessageSummary, mMessageText,
+        Constants.MESSAGES_NOTIFICATION_ID);
+  }
 
-    Notification n = mBuilder.build();
-    mNotificationManager.notify(Constants.MESSAGES_NOTIFICATION_ID, n);
+  private void notify(int smallIconResId,
+      String notificationTitle,
+      String notificationSummary,
+      String notificationText,
+      int notificationId) {
+    NotificationCompat.Builder builder =
+        createNotificationBuilder(smallIconResId, notificationTitle, notificationSummary,
+            notificationText);
+
+    mNotificationManager.notify(notificationId, builder.build());
+  }
+
+  private NotificationCompat.Builder createNotificationBuilder(int smallIconResId,
+      String notificationTitle,
+      String notificationSummary,
+      String notificationText) {
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext).setContentIntent(
+        PendingIntent.getActivity(mContext, 0, mMainActivityIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT))
+        .setPriority(PRIORITY_MAX)
+        .setAutoCancel(true)
+        .setSmallIcon(smallIconResId)
+        .setStyle(createStyle(notificationTitle, notificationSummary, notificationText))
+        .setContentTitle(notificationTitle)
+        .setContentText(notificationText)
+        .setSound(getPreferredRingtoneUri());
+
+    if (shouldVibrate()) {
+      builder.setVibrate(VIBRATION_PATTERN);
+    }
+
+    return builder;
   }
 
   private NotificationCompat.Style createStyle(String title, String summary, String text) {
@@ -97,5 +134,14 @@ public class AppNotifier implements NotifyOnNewMessageInteractor.NotificationDis
     bigTextStyle.bigText(text);
 
     return bigTextStyle;
+  }
+
+  private Uri getPreferredRingtoneUri() {
+    String possibleUri = mSharedPreferences.getString(mRingtonePrefKey, "");
+    return !Objects.equals(possibleUri, "") ? Uri.parse(possibleUri) : null;
+  }
+
+  private boolean shouldVibrate() {
+    return mSharedPreferences.getBoolean(mShouldVibratePrefKey, true);
   }
 }
