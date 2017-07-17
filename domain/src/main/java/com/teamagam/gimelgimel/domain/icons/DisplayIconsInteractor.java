8108795357b332c9f6aba17e50035697005b81a7
Dor <teamagam3@gmail.com>
2017-07-17 15:16:11 +0300

    Added tests

--- domain/src/main/java/com/teamagam/gimelgimel/domain/icons/DisplayIconsInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/icons/DisplayIconsInteractor.java
@@ -4,15 +4,14 @@ import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
-import com.teamagam.gimelgimel.domain.base.interactors.BaseDisplayInteractor;
+import com.teamagam.gimelgimel.domain.base.interactors.BaseSingleDisplayInteractor;
 import com.teamagam.gimelgimel.domain.base.interactors.DisplaySubscriptionRequest;
 import com.teamagam.gimelgimel.domain.icons.entities.Icon;
 import com.teamagam.gimelgimel.domain.icons.repository.IconsRepository;
 import io.reactivex.Observable;
-import java.util.Collections;
 
 @AutoFactory
-public class DisplayIconsInteractor extends BaseDisplayInteractor {
+public class DisplayIconsInteractor extends BaseSingleDisplayInteractor {
 
   private IconsRepository mIconsRepository;
   private Displayer mDisplayer;
@@ -26,12 +25,11 @@ public class DisplayIconsInteractor extends BaseDisplayInteractor {
     mIconsRepository = iconsRepository;
     mDisplayer = displayer;
   }
-
+  
   @Override
-  protected Iterable<SubscriptionRequest> buildSubscriptionRequests(DisplaySubscriptionRequest.DisplaySubscriptionRequestFactory factory) {
-    return Collections.singleton(
-        factory.createSimple(Observable.fromIterable(mIconsRepository.getAvailableIcons()),
-            mDisplayer::display));
+  protected SubscriptionRequest buildSubscriptionRequest(DisplaySubscriptionRequest.DisplaySubscriptionRequestFactory factory) {
+    return factory.createSimple(Observable.fromIterable(mIconsRepository.getAvailableIcons()),
+        mDisplayer::display);
   }
 
   public interface Displayer {
