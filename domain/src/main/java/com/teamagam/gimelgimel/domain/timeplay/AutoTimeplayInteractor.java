efd2b6bf94014d3af286e24e4ed913a9f99a27d6
TeamAgam7 <TeamAgam7@gmail.com>
2017-09-10 18:02:51 +0300

    task is done still have some fixes to do listed with "// todo:"

--- domain/src/main/java/com/teamagam/gimelgimel/domain/timeplay/AutoTimeplayInteractor.java
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/timeplay/AutoTimeplayInteractor.java
@@ -5,6 +5,7 @@ import com.google.auto.factory.Provided;
 import com.teamagam.gimelgimel.domain.base.executor.PostExecutionThread;
 import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
 import io.reactivex.Observable;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @AutoFactory
@@ -14,17 +15,38 @@ public class AutoTimeplayInteractor extends TimeplayInteractor {
 
   public AutoTimeplayInteractor(@Provided ThreadExecutor threadExecutor,
       @Provided PostExecutionThread postExecutionThread,
-      @Provided GeoSnapshoter geoSnapshoter,
-      @Provided GeoTimespanCalculator geoTimespanCalculator,
+      @Provided GeoSnapshoter geoSnapshoter, long startTimeStamp, long endTimeStamp,
       Displayer displayer,
       int intervalCount,
       long initialTimestamp) {
-    super(threadExecutor, postExecutionThread, geoSnapshoter, geoTimespanCalculator, displayer,
+    super(threadExecutor, postExecutionThread, geoSnapshoter,
+        new CustomDatesTimespan(new Date(startTimeStamp), new Date(endTimeStamp)), displayer,
         intervalCount, initialTimestamp);
   }
 
   @Override
   protected Observable<Long> createSourceObservable() {
     return Observable.interval(0, (long) DISPLAY_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
+  }
+
+  public static class CustomDatesTimespan implements GeoTimespanCalculator {
+
+    private Date mStartDate;
+    private Date mEndDate;
+
+    public CustomDatesTimespan(Date startDate, Date endDate) {
+      mStartDate = startDate;
+      mEndDate = endDate;
+    }
+
+    @Override
+    public Date getMinimumGeoItemDate() {
+      return mStartDate;
+    }
+
+    @Override
+    public Date getMaximumGeoItemDate() {
+      return mEndDate;
+    }
   }
 }
