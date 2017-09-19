7394f8ed0af5d0540f78cd67447a7e1b4addf20f
TeamAgam7 <TeamAgam7@gmail.com>
2017-09-19 15:46:00 +0300

    done task

---
+++ domain/src/main/java/com/teamagam/gimelgimel/domain/dynamicLayers/remote/SendRemoteUpdateDescrptionDynamicLayerRequestInteractor.java
@@ -0,0 +1,47 @@
+package com.teamagam.gimelgimel.domain.dynamicLayers.remote;
+
+import com.google.auto.factory.AutoFactory;
+import com.google.auto.factory.Provided;
+import com.teamagam.gimelgimel.domain.base.executor.ThreadExecutor;
+import com.teamagam.gimelgimel.domain.base.interactors.BaseSingleDataInteractor;
+import com.teamagam.gimelgimel.domain.base.interactors.DataSubscriptionRequest;
+import com.teamagam.gimelgimel.domain.dynamicLayers.entity.DynamicLayer;
+import com.teamagam.gimelgimel.domain.dynamicLayers.repository.DynamicLayersRepository;
+import io.reactivex.Observable;
+
+@AutoFactory
+public class SendRemoteUpdateDescrptionDynamicLayerRequestInteractor
+    extends BaseSingleDataInteractor {
+
+  private final DynamicLayerRemoteSourceHandler mDynamicLayerRemoteSourceHandler;
+  private final DynamicLayersRepository mDynamicLayersRepository;
+  private final String mDynamicLayerId;
+  private final String mDescription;
+
+  protected SendRemoteUpdateDescrptionDynamicLayerRequestInteractor(
+      @Provided ThreadExecutor threadExecutor,
+      @Provided DynamicLayerRemoteSourceHandler dynamicLayerRemoteSourceHandler,
+      @Provided DynamicLayersRepository dynamicLayersRepository,
+      String description,
+      String layerId) {
+    super(threadExecutor);
+    mDynamicLayerRemoteSourceHandler = dynamicLayerRemoteSourceHandler;
+    mDynamicLayersRepository = dynamicLayersRepository;
+    mDescription = description;
+    mDynamicLayerId = layerId;
+  }
+
+  @Override
+  protected SubscriptionRequest buildSubscriptionRequest(DataSubscriptionRequest.SubscriptionRequestFactory factory) {
+    return factory.create(Observable.just(mDynamicLayerId),
+        idObservable -> idObservable.doOnNext(this::createAndSendUpdatedDynamicLayer));
+  }
+
+  private void createAndSendUpdatedDynamicLayer(String layerId) {
+    DynamicLayer dynamicLayer = mDynamicLayersRepository.getById(layerId);
+    DynamicLayer updatedDynamicLayer =
+        new DynamicLayer(dynamicLayer.getId(), dynamicLayer.getName(), mDescription, 0,
+            dynamicLayer.getEntities());
+    mDynamicLayerRemoteSourceHandler.updateDescription(updatedDynamicLayer);
+  }
+}
